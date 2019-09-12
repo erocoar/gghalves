b32aa2f87f7ba22870b9c264a96a49c48fbe4f50
Frederik Tiedemann <fj.tiedemann@googlemail.com>
2019-09-13 00:24:21 +0200

    add gghalves.R

--- R/gghalves.R
+++ R/gghalves.R
@@ -1,5 +1,2 @@
-#' @details
-#' D1
-#' D2
-#' @keywords internal
+#' gghalves makes it easy to compose your own half-half plots via ggplot2. Think displaying a boxplot next to jittered points, or violin plots side by side with dotplots.
 "_PACKAGE"
